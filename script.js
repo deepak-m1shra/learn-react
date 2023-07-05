1708460d1767ee3c502f3e1fb64f2a664ec0ac32
Deepak Mishra <deepakmishra@Deepaks-MacBook-Air.local>
2023-07-05 16:18:42 +0530

    basics

---
+++ script.js
@@ -0,0 +1,19 @@
+// const heading = React.createElement("h1", { id: "heading" }, "Hellooo from React!")
+// const reactRoot = ReactDOM.createRoot(document.getElementById("root"))
+// reactRoot.render(heading)
+
+
+setTimeout(() => {
+    const parent = React.createElement("div", {
+        id: "parent"
+    },
+        React.createElement("div", {},
+            [React.createElement("h1", { id: "h1_1" }, "Hello I am h1"),
+            React.createElement("h5", { id: "h5_1" }, "Hello I am h5"),
+            React.createElement("h5", { id: "h5_2" }, "Hello I am another h5")])
+    )
+
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+    root.render(parent)
+}, 4000)
+

98432b7fa2296920c77f2cb4e0ec4f82910aceb9
Deepak Mishra <deepakmishra@Deepaks-MacBook-Air.local>
2023-07-06 20:41:56 +0530

    added parcel support

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 // const reactRoot = ReactDOM.createRoot(document.getElementById("root"))
 // reactRoot.render(heading)
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 
 setTimeout(() => {
     const parent = React.createElement("div", {
@@ -10,7 +13,7 @@ setTimeout(() => {
         React.createElement("div", {},
             [React.createElement("h1", { id: "h1_1" }, "Hello I am h1"),
             React.createElement("h5", { id: "h5_1" }, "Hello I am h5"),
-            React.createElement("h5", { id: "h5_2" }, "Hello I am another h5")])
+            React.createElement("h5", { id: "h5_2" }, "Hello I am another h552231")])
     )
 
     const root = ReactDOM.createRoot(document.getElementById("root"))

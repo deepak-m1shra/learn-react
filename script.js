67263181fdf29831d57877cb10b4a155160f9aca
Deepak Mishra <deepakmishra@Deepaks-MacBook-Air.local>
2023-07-07 15:47:22 +0530

    updates for parcel

--- script.js
+++ script.js
@@ -5,6 +5,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const timeOut1 = 1000
+const timeOut2 = 3000
+
+const style = {
+    color: "green"
+}
+
+const parent1 =
+    (<>
+        <h1 id="jsxParent">This is Parent (React Element in green color)(JSX) </h1>
+        <div>
+            <h1>Hello I am h1 (JSX)</h1>
+            <h5>Hello I am h5 (JSX)</h5>
+            <h5>Hello I am another h5 (JSX)</h5>
+            <p>Values of timeout1 and timeOut2 are: {timeOut1}, {timeOut2}</p>
+        </div>
+    </>);
 
 setTimeout(() => {
     const parent = React.createElement("div", {
@@ -18,5 +35,27 @@ setTimeout(() => {
 
     const root = ReactDOM.createRoot(document.getElementById("root"))
     root.render(parent)
-}, 4000)
+}, timeOut1)
+
+const ContentComponent = () => <div>This is ContentComponent</div>
+
+const HeadingComponent = () =>
+    <><h1>This is Heading component </h1>
+        <h5>Including React element in React Component</h5>
+        <div style={style}>{parent1}</div>
+        <p>Values of timeout1 and timeOut2 are: {timeOut1}, {timeOut2}</p>
+        <ContentComponent />
+    </>
+
+
+setTimeout(() => {
+
+    const root1 = ReactDOM.createRoot(document.getElementById("root"))
+
+    // Rendering Elements
+    // root1.render(parent1)
+
+    // Rendering Components
+    root1.render(<HeadingComponent />)
+}, timeOut2)
 
